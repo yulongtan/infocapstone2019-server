1c63316d489d75be3eff8b5965ffab2c3d15d057
Phillip Park <phillipypark@gmail.com>
2019-02-26 22:31:53 -0800

    Firebase is functional. Create a few functions.

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 let admin = require('firebase-admin');
 
+
+
+
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.firebase_project_id || require('./firebase_auth.json').project_id,
-    clientEmail: process.env.firebase_client_email || require('./firebase_auth.json').client_email,
-    privateKey: (process.env.firebase_private_key || require('./firebase_auth.json').private_key).replace(/\\n/g, '\n'),
-  }),
-  databaseURL: process.env.firebase_db_url || require('./auth.json').db_url
+    credential: admin.credential.cert({
+        projectId: process.env.firebase_project_id || require('./firebase_auth.json').project_id,
+        clientEmail: process.env.firebase_client_email || require('./firebase_auth.json').client_email,
+        privateKey: (process.env.firebase_private_key || require('./firebase_auth.json').private_key).replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.firebase_db_url || require('./auth.json').db_url
 });
+
+var db = admin.database();
+
 
 const app = express();
 
@@ -20,52 +27,64 @@ let scraper = require('./scraper');
 
 // Middleware to fill the request body
 let rawBodySaver = (req, res, buf, encoding) => {
-  if (buf && buf.length) {
-    req.rawBody = buf.toString(encoding || 'utf8');
-  }
+    if (buf && buf.length) {
+        req.rawBody = buf.toString(encoding || 'utf8');
+    }
 }
 
-app.use(bodyParser.json({ verify: rawBodySaver }));
-app.use(bodyParser.urlencoded({ verify: rawBodySaver, extended: true }));
-app.use(bodyParser.raw({ verify: rawBodySaver, type: function () { return true } }));
+app.use(bodyParser.json({
+    verify: rawBodySaver
+}));
+app.use(bodyParser.urlencoded({
+    verify: rawBodySaver,
+    extended: true
+}));
+app.use(bodyParser.raw({
+    verify: rawBodySaver,
+    type: function () {
+        return true
+    }
+}));
 
 app.post('/sms', async (req, res) => {
-  const twiml = new MessagingResponse();
+    const twiml = new MessagingResponse();
 
-  const prefix = '!';
-  /**
-   * IMPORTANT: USE req.body.Body for production and req.rawBody for local testing
-   */
-  let message = req.body.Body;
-  //let message = req.rawBody;
-  if (message.startsWith(prefix)) {
-    let args = message.slice(prefix.length).trim().split(/ +/g);
-    let command = args.shift().toLowerCase();
+    const prefix = '!';
+    /**
+     * IMPORTANT: USE req.body.Body for production and req.rawBody for local testing
+     */
+    let message = req.body.Body;
+    //let message = req.rawBody;
+    if (message.startsWith(prefix)) {
+        let args = message.slice(prefix.length).trim().split(/ +/g);
+        let command = args.shift().toLowerCase();
 
-    if (command === 'test') {
-      console.log('Test was sent');
-      twiml.message('Test was sent');
+        if (command === 'test') {
+            console.log('Test was sent');
+            twiml.message('Test was sent');
+        }
+
+        if (command === 'drives') {
+            let zip = args[0];
+            console.log(zip);
+            if (!zip) {
+                console.log('no zip');
+                twiml.message('Please input a zip code. Usage: !drives <zipcode>');
+            } else {
+                let drives = await scraper.getTimes(zip);
+                twiml.message(JSON.stringify(drives, null, 2));
+            }
+        }
     }
+    //twiml.message(`You typed: ${message}`);
 
-    if (command === 'drives') {
-      let zip = args[0];
-      console.log(zip);
-      if (!zip) {
-        console.log('no zip');
-        twiml.message('Please input a zip code. Usage: !drives <zipcode>');
-      } else {
-        let drives = await scraper.getTimes(zip);
-        twiml.message(JSON.stringify(drives, null, 2));
-      }
-    }
-  }
-  //twiml.message(`You typed: ${message}`);
-
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.end(twiml.toString());
+    res.writeHead(200, {
+        'Content-Type': 'text/xml'
+    });
+    res.end(twiml.toString());
 });
 
 let port = process.env.PORT || 5000;
 http.createServer(app).listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+    console.log(`Express server listening on port ${port}`);
 });
---
+++ testFireBase.js
@@ -0,0 +1,80 @@
+let admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.firebase_project_id || require('./firebase_auth.json').project_id,
+        clientEmail: process.env.firebase_client_email || require('./firebase_auth.json').client_email,
+        privateKey: (process.env.firebase_private_key || require('./firebase_auth.json').private_key).replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.firebase_db_url || require('./auth.json').firebase_db_url
+});
+
+var db = admin.database();
+
+// var ref = db.ref("restricted_access/secret_document");
+// ref.once("value", function (snapshot) {
+//     console.log(snapshot.val());
+// });
+
+// param: phone number is grabbed from twilio
+// Descr: Stores phone number as ID and creates a new user in firecase
+function writeUserData(phoneNumber) {
+    db.ref('users/' + phoneNumber).set({
+        bloodType: "", // String
+        numberOfTimesDonated: 0, // Integer
+        numOfPints: 0.0, // Double?
+        estimatedLivesSaved: 0, // Integer
+        bloodDrawnDate: "", // String or Date 
+        // Calculate bloodDrawnDate to find eligibleDate?
+        // add 56 days to bloodDrawnDdate to get eligibleDate
+        //  eligibleDate: "", // String or Date
+        donated: false, // boolean 
+        // first time donor = false, otherwise true
+        pre_assessment_result: "",
+        // String (Pass/Failed/Flagged)
+        pre_assessment_questions: [""],
+        // Array of questions of concern
+        zipcode: [""]
+    });
+}
+
+// read user data from firebase 
+// var userId = firebase.auth().currentUser.uid;
+// return firebase.database().ref('/users/' + userId).once('value').then(function (snapshot) {
+//     var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+//     // ...
+// });
+
+// Reads the data associated with a given phone number and console logs it
+function grabAllUser(phoneNumber) {
+    var ref = db.ref("/users/" + phoneNumber);
+    ref.on("value", function (snapshot) {
+        console.log(snapshot.val());
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+}
+
+// Retrieve's users' statistics by using the phone Number ID
+function grabUserStats(phoneNumber) {
+    var ref = db.ref("/users/" + phoneNumber);
+    ref.orderByChild(phoneNumber).on("child_added", function (snapshot) {
+        console.log(snapshot.val().bloodType);
+
+    });
+
+
+
+
+    ref.orderByChild(phoneNumber).on("child_added", function (snapshot) {
+        //console.log(snapshot.key + " was " + snapshot.val().height + " meters tall");
+        console.log("Blood Type: " + snapshot.val().bloodType + ", Number of Times Donated: " + snapshot.val().numberOfTimesDonated + ", Number of Pints: " + snapshot.val().numOfPints + ", Lives Saved: " + snapshot.val().estimatedLivesSaved);
+        // add next eligibility date
+    });
+}
+
+grabUserStats("253-555-8645");
+
+// function justDonated(phoneNumber) {
+
+// }
