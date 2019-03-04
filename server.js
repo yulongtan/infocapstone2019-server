3d48d5fe0ca93b9de1b0d66ff9a1879004dc0bd0
Phillip Park <phillipypark@gmail.com>
2019-03-03 22:33:06 -0800

    Modified firebasehelper file and server file

--- server.js
+++ server.js
@@ -10,100 +10,113 @@ let firebaseHelper = require('./db/FirebaseHelper');
 
 // Middleware to fill the request body
 let rawBodySaver = (req, res, buf, encoding) => {
-  if (buf && buf.length) {
-    req.rawBody = buf.toString(encoding || 'utf8');
-  }
+    if (buf && buf.length) {
+        req.rawBody = buf.toString(encoding || 'utf8');
+    }
 }
 
 app.use(bodyParser.json({
-  verify: rawBodySaver
+    verify: rawBodySaver
 }));
 app.use(bodyParser.urlencoded({
-  verify: rawBodySaver,
-  extended: true
+    verify: rawBodySaver,
+    extended: true
 }));
 app.use(bodyParser.raw({
-  verify: rawBodySaver,
-  type: function () {
-    return true
-  }
+    verify: rawBodySaver,
+    type: function () {
+        return true
+    }
 }));
 
 app.post('/sms', async (req, res) => {
-  const twiml = new MessagingResponse();
+    const twiml = new MessagingResponse();
 
-  const prefix = '!';
-  /**
-   * IMPORTANT: USE req.body.Body for production and req.rawBody for local testing
-   */
-  console.log(JSON.stringify(req.body, null, 2));
-  console.log(`Number: ${req.body.From}`);
-  let message = req.body.Body;
-  //let message = req.rawBody;
-  if (message.startsWith(prefix)) {
-    let args = message.slice(prefix.length).trim().split(/ +/g);
-    let command = args.shift().toLowerCase();
+    const prefix = '!';
+    /**
+     * IMPORTANT: USE req.body.Body for production and req.rawBody for local testing
+     */
+    console.log(JSON.stringify(req.body, null, 2));
+    console.log(`Number: ${req.body.From}`);
+    let message = req.body.Body;
+    //let message = req.rawBody;
+    if (message.startsWith(prefix)) {
+        let args = message.slice(prefix.length).trim().split(/ +/g);
+        let command = args.shift().toLowerCase();
 
-    if (command === 'test') {
-      console.log('Test was sent');
-      twiml.message('Test was sent');
-    }
-
-    if (command === 'drives') {
-      let zip = args[0];
-      console.log(zip);
-      if (!zip) {
-        console.log('no zip');
-        twiml.message('Please input a zip code. Usage: !drives <zipcode>');
-      } else {
-        let drives = await scraper.getTimes(zip);
-        if (!drives) {
-          twiml.message(`Could not retrieve blood drives for zip code ${zip}`);
-        } else {
-          twiml.message(drives);
+        if (command === 'test') {
+            console.log('Test was sent');
+            twiml.message('Test was sent');
         }
-      }
+
+        if (command === 'drives') {
+            let zip = args[0];
+            console.log(zip);
+            if (!zip) {
+                console.log('no zip');
+                twiml.message('Please input a zip code. Usage: !drives <zipcode>');
+            } else {
+                let drives = await scraper.getTimes(zip);
+                if (!drives) {
+                    twiml.message(`Could not retrieve blood drives for zip code ${zip}`);
+                } else {
+                    twiml.message(drives);
+                }
+            }
+        }
+
+        // registers new users
+        if (command === 'register') {
+            //let phoneNumber = ""; // for tesing locally
+            let phoneNumber = req.body.From;
+            let registered = await firebaseHelper.createNewUser(phoneNumber);
+            console.log(registered);
+            let message = '';
+            if (registered) {
+                message = 'You have been registered! Here is a list of available commands:\n!drives <zipcode>: Gets nearby blood drives\n!stats: Gets your statistics\n!eligibility: Get your next eligibility date\n!donated: Use to command to mark that you donated';
+            } else {
+                message = 'This number has already been registered.'
+            }
+            twiml.message(message);
+        }
+
+        // return user stats
+        if (command === 'stats') {
+            let phoneNumber = '253'; // for tesing locally
+            // let phoneNumber = req.body.From; Add this back for production s
+            let exists = firebaseHelper.userExists(phoneNumber);
+            let message = '';
+            if (exists) {
+                let userStats = await firebaseHelper.getUserStats(phoneNumber);
+                message = ''; // do something with userstats
+            } else {
+                message = 'Looks like you have not registered yet!';
+            }
+            twiml.message(message);
+        }
+
+        // just Donated
+        if (command === 'donated') {
+            let phoneNumber = '253';
+            let exists = firebaseHelper.userExists(phoneNumber);
+            let message = '';
+            if (exists) {
+                let donated = await firebaseHelper.justDonated(phoneNumber);
+                message = ''; // do something with donated;
+            } else {
+                message = 'Looks like you have not registered yet!';
+            }
+            twiml.message(message);
+        }
     }
 
-    // registers new users
-    if (command === 'register') {
-      //let phoneNumber = ""; // for tesing locally
-      let phoneNumber = req.body.From; 
-      let registered = await firebaseHelper.createNewUser(phoneNumber);
-      console.log(registered);
-      let message = '';
-      if (registered) {
-        message = 'You have been registered! Here is a list of available commands:\n!drives <zipcode>: Gets nearby blood drives\n!stats: Gets your statistics\n!eligibility: Get your next eligibility date\n!donated: Use to command to mark that you donated';
-      } else {
-        message = 'This number has already been registered.'
-      }
-      twiml.message(message);
-    }
-
-    // return user stats
-    if (command === 'stats') {
-      let phoneNumber = '253'; // for tesing locally
-      // let phoneNumber = req.body.From; Add this back for production s
-
-      let userStats = await firebaseHelper.getUserStats(phoneNumber);
-      console.log(userStats);
-    }
-
-    // just Donated
-    if (command === 'donated') {
-      let phoneNumber = '253';
-      let donated = await firebaseHelper.justDonated(phoneNumber);
-      await console.log(donated);
-    }
-  }
-
-  res.writeHead(200, {
-    'Content-Type': 'text/xml'
-  });
-  res.end(twiml.toString());
+    res.writeHead(200, {
+        'Content-Type': 'text/xml'
+    });
+    res.end(twiml.toString());
 });
 
 let port = process.env.PORT || 5000;
 http.createServer(app).listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+    console.log(`Express server listening on port ${port}`);
 });
